8f70267dd015647170d4e3afc1782aebeae6d27c
Sıla's MacBook Air <silasmacbookair@192.168.1.105>
2026-02-01 15:39:17 +0300

    Initial Portfolio

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+      <nav className="home-nav-bar">
+        <a href="#about">Hakkımda |</a>   
+        <a href="#projects"> Projelerim |</a>  
+        <a href="#contact"> İletişim |</a>
+      </nav>
+    )
+  }
+  
